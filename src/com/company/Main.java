66deb2316c7bee57ebde91411f350eca0d43b19c
Anthony Valle <valleganthony@gmail.com>
2019-05-03 15:42:10 -0400

    made new method usergui. Made while loop to create new users but it did not work.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,8 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    public static userGui(){
 
+    public static void main(String[] args) {
         // Person temp vars
         String tempName;
         String tempEmail;
@@ -33,6 +33,11 @@ public class Main {
         String tempSkillthree;
         String tempLevelSkillThree;
 
+
+        //Loop vars
+        String newUser;
+        boolean switchU = true;
+
         //ArrayList of Users
         ArrayList<Person> users = new ArrayList<Person>();
 
@@ -40,117 +45,90 @@ public class Main {
         Scanner keyboard = new Scanner(System.in);
 
 
-            //Method should start here
-            System.out.println("Hello, What is your name?");
-            tempName = keyboard.nextLine();
-            System.out.println("What is your email?");
-            tempEmail = keyboard.nextLine();
+        System.out.println("Hello, What is your name?");
+        tempName = keyboard.nextLine();
+        System.out.println("What is your email?");
+        tempEmail = keyboard.nextLine();
 
-            // Asking for education vars to make an instance
-            System.out.println("Tell us about your education, What is the highest degree that you currently hold?");
-            tempDegreeType = keyboard.nextLine();
-            System.out.println("What was your major?");
-            tempMajor = keyboard.nextLine();
-            System.out.println("what was the name of your university?");
-            tempUniversityName = keyboard.nextLine();
-            System.out.println("What year did you graduate?");
-            tempGraduationYear = keyboard.nextLine();
+        // Asking for education vars to make an instance
+        System.out.println("Tell us about your education, What is the highest degree that you currently hold?");
+        tempDegreeType = keyboard.nextLine();
+        System.out.println("What was your major?");
+        tempMajor = keyboard.nextLine();
+        System.out.println("what was the name of your university?");
+        tempUniversityName = keyboard.nextLine();
+        System.out.println("What year did you graduate?");
+        tempGraduationYear = keyboard.nextLine();
 
         /*
         Making the instance of the education and calling it after that person  + education
          */
-            Education u1 = new Education(tempDegreeType, tempMajor, tempUniversityName, tempGraduationYear);
+        Education u1 = new Education(tempDegreeType,tempMajor,tempUniversityName,tempGraduationYear);
 
-            // EDUCATION IS DONE
+        // EDUCATION IS DONE
 
-            //Start of the job section
-            System.out.println("Thank you! Let's start working on your experience ");
-            System.out.println("Please enter the name of your most recent employer:");
-            tempCompany = keyboard.nextLine();
-            System.out.println("Nice! What was your job title?");
-            tempJobTitle = keyboard.nextLine();
-            System.out.println("Please enter your Start date (Month,Year)");
-            tempStartDate = keyboard.nextLine();
-            System.out.println("Please enter your end date (Month,Year)");
-            tempEndDate = keyboard.nextLine();
-            System.out.println("Please enter a description of your role");
-            tempJobDescription = keyboard.nextLine();
+        //Start of the job section
+        System.out.println("Thank you! Let's start working on your experience ");
+        System.out.println("Please enter the name of your most recent employer:");
+        tempCompany = keyboard.nextLine();
+        System.out.println("Nice! What was your job title?");
+        tempJobTitle =keyboard.nextLine();
+        System.out.println("Please enter your Start date (Month,Year)");
+        tempStartDate = keyboard.nextLine();
+        System.out.println("Please enter your end date (Month,Year)");
+        tempEndDate = keyboard.nextLine();
+        System.out.println("Please enter a description of your role");
+        tempJobDescription = keyboard.nextLine();
 
-            //Making an instance of JOB with user Input
-            Job j1 = new Job(tempCompany, tempJobTitle, tempStartDate, tempEndDate, tempJobDescription);
+        //Making an instance of JOB with user Input
+        Job j1 = new Job(tempCompany,tempJobTitle,tempStartDate,tempEndDate,tempJobDescription);
 
-            // JOB SECTION IS DONE
+        // JOB SECTION IS DONE
 
-            //Start of the skills section
+        //Start of the skills section
 
-            System.out.println("Let's add some skills to your resume");
-            System.out.println("What is the your first skill? (this could be something like python)");
-            tempSkillOne = keyboard.nextLine();
-            System.out.println("Proficiency level(Fundamental, Novice, Intermediate, Advanced)");
-            tempLevelSkillOne = keyboard.nextLine();
-            System.out.println("What is the name of your second skill:");
-            tempSkillTwo = keyboard.nextLine();
-            System.out.println("Proficiency level:(Fundamental, Novice, Intermediate, Advanced");
-            tempLevelSkillTwo = keyboard.nextLine();
-            System.out.println("What is the name of your third skill:");
-            tempSkillthree = keyboard.nextLine();
-            System.out.println("Proficiency level:(Fundamental, Novice, Intermediate, Advanced");
-            tempLevelSkillThree = keyboard.nextLine();
-            System.out.println("WE ARE DONE!");
+        System.out.println("Let's add some skills to your resume");
+        System.out.println("What is the your first skill? (this could be something like python)");
+        tempSkillOne = keyboard.nextLine();
+        System.out.println("Proficiency level(Fundamental, Novice, Intermediate, Advanced)");
+        tempLevelSkillOne = keyboard.nextLine();
+        System.out.println("What is the name of your second skill:");
+        tempSkillTwo = keyboard.nextLine();
+        System.out.println("Proficiency level:(Fundamental, Novice, Intermediate, Advanced");
+        tempLevelSkillTwo = keyboard.nextLine();
+        System.out.println("What is the name of your third skill:");
+        tempSkillthree = keyboard.nextLine();
+        System.out.println("Proficiency level:(Fundamental, Novice, Intermediate, Advanced");
+        tempLevelSkillThree = keyboard.nextLine();
+        System.out.println("WE ARE DONE!");
 
-            // Making an instance of Skills
-            Skill s1 = new Skill(tempSkillOne, tempLevelSkillOne, tempSkillTwo, tempLevelSkillTwo, tempSkillthree, tempLevelSkillThree);
+        // Making an instance of Skills
+        Skill s1 = new Skill(tempSkillOne,tempLevelSkillOne,tempSkillTwo,tempLevelSkillTwo,tempSkillthree,tempLevelSkillThree);
 
-            //END OF SKILLS
+        //END OF SKILLS
 
         /*
         Now making an instance of a person
          */
 
-            Person tempname = new Person(tempName, tempEmail, u1, j1, s1);
-            users.add(tempname);
+        Person tempname = new Person(tempName,tempEmail,u1,j1,s1);
+        //Adding the user to the arraylist for users
+        users.add(tempname);
 
-            users.get(0).printPersonInfo();
-            users.get(1).printPersonInfo();
+        //The function below displays the resume for the current user.
+        tempname.printPersonInfo();
 
+        //System.out.println("Would you like to add another user (Y/N)?");
+        //newUser = keyboard.nextLine();
 
-            //The function below displays the resume for the current user.
-            tempname.printPersonInfo();
+//        if(newUser.equalsIgnoreCase("Y")){
+//            System.out.println("This will create a new user");
+//        }
+//
+//        else{
+//            switchU = false;
+//        }
 
-
-        }
-
-
-
-
-
-
-    public static void main(String[] args) {
-        //Loop vars
-        String newUser;
-        boolean switchU = true;
-
-        Scanner keymain = new Scanner(System.in);
-
-        System.out.println("Would you like to create a resume?(Y/N)");
-        newUser = keymain.nextLine();
-
-        if (newUser.equalsIgnoreCase("Y")){
-            while (switchU ==true){
-                userGui();
-                System.out.println("Would you like to add another user (Y/N)?");
-                newUser = keymain.nextLine();
-                if(newUser.equalsIgnoreCase("Y")){
-                    switchU = true;
-                    userGui();
-                }
-
-                else{
-                    switchU = false;
-                }
-            }
-
-        }
 
 
     }
